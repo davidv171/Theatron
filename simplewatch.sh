b2d25219541d0e568d89364b479d9dc309aa6948
David <david.vuckovic7@gmail.com>
2018-09-02 20:14:10 +0200

    0.1: Should now be launchable from a terminal, that is not cd into the script directory

--- simplewatch.sh
+++ simplewatch.sh
@@ -1,4 +1,5 @@
 quality="best\n720p60\n720p\n480p\n360p\naudio_only"
+cd $(dirname "$0")
 oauth="$(cat oauth|tr -d '\n')"
 channel=$(curl -s -H 'Accept: application/vnd.twitchtv.v5+json' \
     -H 'Client-ID: fendbm5b5q1c2820m59sbdv9z95vs4' \
