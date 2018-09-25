c32167549e3c47352f4e2aede4b4669cb42bd136
David <david.vuckovic7@gmail.com>
2018-09-25 15:32:41 +0200

    Updated readme, less oauth requets,better config, added player

--- config.sh
+++ config.sh
@@ -1,15 +1,14 @@
 #dont ask for quality, always just run the best one, always worst might be implemented if anyone asks
-always_best="false"
+always_best=false
 
 #The default player used by streamlink with the latest breakage of VLC we can't rely on it anymore :(
 player="mpv"
 
 #if this is on, every time you pick a streamer, you will get vod menu after you pick a streamer, the first entry should still be the live stream
-vod_mode="true"
+vod_mode=true
+
 # How many vods(filtered by recency) to show
 vod_mode_limit="25"
 
-
-
  #if true, opens up a browser window with the chat popup mode of the picked streamer, does so not knowingly if the stream loaded or not
-popup_chat="false"
+popup_chat=false
