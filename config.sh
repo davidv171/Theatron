6ada87f273f7d29c8ebe8da5540ccf97c1c653a8
David <david.vuckovic7@gmail.com>
2018-11-08 12:42:09 +0100

    Got rid of streamlink dependency... needs mpv and youtube-dl now

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 always_best=false
 
 #The default player used by streamlink with the latest breakage of VLC we can't rely on it anymore :(
-player="mpv"
+player="vlc"
 
 #if this is on, every time you pick a streamer, you will get vod menu after you pick a streamer, the first entry should still be the live stream
 vod_mode=true
